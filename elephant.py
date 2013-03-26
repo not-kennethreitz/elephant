502f6ae10dbd51c6cea570a2aaff63f87b3e2b6c
Kenneth Reitz <me@kennethreitz.com>
2013-03-25 22:58:40 -0400

    create buckets automatically

--- elephant.py
+++ elephant.py
@@ -3,7 +3,10 @@
 import os
 from uuid import uuid4
 
+import boto
 from flask import Flask, request
+# from boto.s3.connection import S3Connection
+# from boto.exception import S3ResponseError
 from pyelasticsearch import ElasticSearch
 
 
@@ -13,6 +16,12 @@ app = Flask(__name__)
 ELASTICSEARCH_URL = os.environ['ELASTICSEARCH_URL']
 CLUSTER_NAME = os.environ['CLUSTER_NAME']
 API_KEY = os.environ['API_KEY']
+
+# If S3 bucket doesn't exist, set it up.
+BUCKET_NAME = 'elephant-{}'.format(CLUSTER_NAME)
+BUCKET = boto.connect_s3().create_bucket(BUCKET_NAME)
+
+
 
 @app.before_request
 def require_apikey():
